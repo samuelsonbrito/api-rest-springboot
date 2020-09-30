f867156fa31f2f16a6b900fe8cee83c038c4c89a
Samuelson <samuelsonma@gmail.com>
2020-09-30 15:48:24 -0400

    estrutura do projeto

---
+++ src/test/java/br/org/creathus/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.creathus.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
