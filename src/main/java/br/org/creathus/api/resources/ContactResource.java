0cb3d54e882f295d5e9e48681f56aa5715f7139d
Samuelson <samuelsonma@gmail.com>
2020-09-30 23:55:09 -0400

    documentação da api com swagger

--- src/main/java/br/org/creathus/api/resources/ContactResource.java
+++ src/main/java/br/org/creathus/api/resources/ContactResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,34 +17,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.org.creathus.api.models.Contact;
 import br.org.creathus.api.repository.ContactRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/api")
+@Api(value="API REST Contatos")
 public class ContactResource {
 	
 	@Autowired
 	ContactRepository contactRepository;
 	
+	@ApiOperation(value="Retorna lista de contatos")
 	@GetMapping("contatos")
 	public List<Contact> findAll(){
 		return contactRepository.findAll();
 	}
 	
+	@ApiOperation(value="Retorna contato único")
 	@GetMapping("contato/{id}")
 	public Contact findById(@PathVariable(value="id") long id){
 		return contactRepository.findById(id);
 	}
 	
+	@ApiOperation(value="Salva contato")
 	@PostMapping("contato")
 	public Contact save(@RequestBody @Valid Contact contact) {
 		return contactRepository.save(contact);
 	}
 	
+	@ApiOperation(value="Deleta contato")
 	@DeleteMapping("contato")
 	public void dalete(@RequestBody Contact contact) {
 		contactRepository.delete(contact);
 	}
 	
+	@ApiOperation(value="Atualiza contato")
 	@PutMapping("contato")
 	public Contact update(@RequestBody @Valid Contact contact) {
 		return contactRepository.save(contact);
