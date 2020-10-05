8b4b518619bcc5b30eb0d4ba69171e3fb0967a89
Samuelson <samuelsonma@gmail.com>
2020-10-04 20:34:58 -0400

    sql do formato simplificado e não criar banco pel jpa

--- src/main/java/br/org/creathus/api/models/Contact.java
+++ src/main/java/br/org/creathus/api/models/Contact.java
@@ -18,7 +18,7 @@ public class Contact implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	@NotNull
